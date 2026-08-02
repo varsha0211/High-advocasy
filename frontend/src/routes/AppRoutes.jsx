d92c83d5dc03e482755ea66e29ba687b7dcf9fec
varsha0211 <varshapanchal211@gmail.com>
2026-08-02 16:37:30 +0530

    resolve vercel error

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -7,10 +7,9 @@ import Wall from "../pages/PublicWall";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/submit" element={<SubmitTestimonial />}></Route>
-      <Route path="/dashboard" element={<Dashboard />}></Route>
-      <Route path="/wall" element={<Wall />}></Route>
-      <Route path="/" element={<Navigate to="/submit" replace />} />
+      <Route path="/" element={<SubmitTestimonial />} />
+      <Route path="/wall" element={<Wall />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="*" element={<div>Page Not Found</div>} />
     </Routes>
   );
